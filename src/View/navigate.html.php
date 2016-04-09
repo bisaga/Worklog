6a056a9432cbe948cb21f1883a1baabd7697a3c6
bisaga <igor.babic@bisaga.com>
2016-04-09 22:34:38 +0200

    Add ag-grid add test persons server side

--- src/View/navigate.html.php
+++ src/View/navigate.html.php
@@ -1,51 +1,45 @@
 <div id="navigate">
     <nav class="navbar navbar-default navbar-static-top" role="navigation">
-        <div class="navbar-header">
-            <a class="navbar-brand" href=".">
-                <img alt="Bisaga" src="images/w24x24.png"/>
-            </a>
-            <a class="navbar-brand" href=".">
-                Worklog v1.0
-            </a>
-        </div>
-        <div>
-            <ul class="nav navbar-top-links navbar-right">
-                <li>
-                    <a href="workday">
-                        <i class="fa fa-calendar-o fa-fw"></i> Workday
-                    </a>
-                </li>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed"
+                        data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=".">
+                    <img alt="Bisaga" src="images/w24x24.png"/>
+                </a>
+                <a class="navbar-brand" href=".">
+                    Worklog
+                </a>
+            </div>
 
-                <li class="dropdown">
-                    <a  href="notimplemented">
-                        <i class="fa fa-bell fa-fw"></i> Alerts
-                    </a>
-                </li>
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i><i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="notimplemented"><i class="fa fa-user fa-fw"></i> User Profile</a></li>
-                        <li><a href="notimplemented"><i class="fa fa-gear fa-fw"></i> Settings</a></li>
-                        <li class="divider"></li>
-                        <li><a href="person"><i class="fa fa-users fa-fw"></i> Persons</a></li>
-                        <li class="divider"></li>
-                        <li><a href="notimplemented"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-navicon fa-fw"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="notimplemented"><i class="fa fa-tasks fa-fw"></i> Task management</a></li>
-                        <li><a href="notimplemented"><i class="fa fa-calendar fa-fw"></i> Calendar </a></li>
-                        <li><a href="notimplemented"><i class="fa fa-area-chart fa-fw"></i> Reports</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="workday">
+                            <i class="fa fa-calendar-o fa-fw"></i> Workday
+                        </a>
+                    </li>
+                    <li class="dropdown" >
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-user fa-fw"></i><i class="fa fa-caret-down"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="notimplemented"><i class="fa fa-user fa-fw"></i> User Profile</a></li>
+                            <li><a href="notimplemented"><i class="fa fa-gear fa-fw"></i> Settings</a></li>
+                            <li class="divider"></li>
+                            <li><a href="person"><i class="fa fa-users fa-fw"></i> Persons</a></li>
+                            <li class="divider"></li>
+                            <li><a href="notimplemented"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </div>
--- src/View/workday.html.php
+++ src/View/workday.html.php
@@ -7,16 +7,22 @@
 <?php $this->import('navigate.html.php') ?>
 
 <div class="toolbarline">
-    <button id="addNew" class="btn btn-default">Add new</button>
+    <button id="addNew" class="btn btn-default">Add</button>
 </div>
 
 <div class="container">
-        <label>Workday</label>
-        <input id="selectedDate">
-        <label>Arrive</label>
-        <input id="startTime">
-        <label>Leave</label>
-        <input id="endTime">
+    <row>
+        <div class="col-xs-12 col-sm-2">Calendar</div>
+        <div class="col-xs-12 col-sm-5" >Grid</div>
+        <div class="col-xs-12 col-sm-5">Edit form
+            <label>Workday</label>
+            <input id="selectedDate">
+            <label>Arrive</label>
+            <input id="startTime">
+            <label>Leave</label>
+            <input id="endTime">
+        </div>
+    </row>
 </div>
 <div id="gridDay" style="width:100%; height:250px"></div>
 
