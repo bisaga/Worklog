40233b6c7ff89976c3e00acfea5de54edc77a9c8
bisaga <igor.babic@bisaga.com>
2016-04-23 13:00:02 +0200

    Organize javascript client code into AMD modules with RequireJS.

--- web/script/app/workday.js
+++ web/script/app/workday.js
@@ -6,98 +6,37 @@
  */
 define(function (require) {
   'use strict';
+  var $ = require('jquery');
   require('bootstrap');
   require('datetimepicker');
-  require('clockpicker')
-  var $ = require('jquery');
-  var agGrid = require('aggrid');
+  require('clockpicker');
+
+  var DayTasksGrid = require('./day_tasks_grid');
 
   // document ready function
   $(function () {
-    var rowData = [];
-
-    var columnDefs = [
-      {headerName: 'Id', field: 'id', width: 75},
-      {headerName: 'Time', field: 'logtime', width: 100},
-      {headerName: 'Description', field: 'description', width: 250},
-      {headerName: 'Task', field: 'taskcode', width: 200}
-    ];
-
-    var gridDiv = $('#grid').get(0);
-
-    var gridOptions = {
-      enableColResize: true,
-      headerHeight: 30,
-      rowHeight: 30,
-      rowSelection: 'single',
-      onSelectionChanged: gridOnSelectionChanged,
-      columnDefs: columnDefs,
-      rowData: rowData,
-      virtualPaging: true,
-      rowModelType: 'virtual'
-    };
-
-    function gridOnSelectionChanged() {
-      var selectedId;
-      var selectedRows = gridOptions.api.getSelectedRows();
-      selectedRows.forEach(function (selectedRow, index) {
-        selectedId = selectedRow.id;
-        return selectedId;
+    /**
+     * Init page function
+     */
+    var initPage = function () {
+      // create calendar
+      $('#selectedDate').datetimepicker({
+        inline: true,
+        format: 'MM.DD.YYYY',
+        locale: 'en'
       });
-      alert('You selected: ' + selectedId);
-    }
 
-    function reloadData() {
-      $.ajax({
-        type: 'GET',
-        url: '/workday_grid',
-        success: function (data) {
-          setRowData(data);
-        },
-        error: function (event, request, settings) {
-          console.log('Error ajax request: ' + settings.url);
-        }
+      // create clock picker out of divs with 'clockpicker' css class
+      $('.clockpicker').clockpicker({
+        align: 'left',
+        donetext: 'Select'
       });
+
+      // create grid
+      var grid = new DayTasksGrid($('#grid').get(0));
+      grid.initialise();
     }
 
-    function setRowData(allOfTheData) {
-      var dataSource = {
-        rowCount: null, // behave as infinite scroll
-        pageSize: 100,
-        maxConcurrentRequest: 2,
-        maxPagesInCache: 2,
-
-        getRows: function (params) {
-          var rowsThisPage = allOfTheData.slice(params.startRow, params.endRow);
-          // if on or after the last page, work out the last row.
-          var lastRow = -1;
-          if (allOfTheData.length <= params.endRow) {
-            lastRow = allOfTheData.length;
-          }
-          // call the success callback
-          params.successCallback(rowsThisPage, lastRow);
-        }
-      };
-      gridOptions.api.setDatasource(dataSource);
-    }
-
-    // create calendar
-    $('#selectedDate').datetimepicker({
-      inline: true,
-      format: 'MM.DD.YYYY',
-      locale: 'en'
-    });
-
-    // create clock picker out of divs with 'clockpicker' css class
-    $('.clockpicker').clockpicker({
-      align: 'left',
-      donetext: 'Select'
-    });
-
-    // create grid
-    agGrid.Grid(gridDiv, gridOptions);
-
-    // populate grid with data
-    reloadData();
+    initPage();
   });
 });
